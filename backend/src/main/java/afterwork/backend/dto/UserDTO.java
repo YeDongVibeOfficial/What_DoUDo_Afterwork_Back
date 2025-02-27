1e5c3f4cc2efac68be590e91dbff33df530126ac
YeDongVibe <wkwhstladmss@naver.com>
2025-02-27 19:21:14 +0900

    250226_DTO ing

---
+++ backend/src/main/java/afterwork/backend/dto/UserDTO.java
@@ -0,0 +1,18 @@
+//DTD : 계층 간 (Controller, View 등) 데이터 교환 위한 Java bin. -> 로직 없는 순수한 데이터 객체
+package afterwork.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    int ID;
+    String
+
+    
+}
