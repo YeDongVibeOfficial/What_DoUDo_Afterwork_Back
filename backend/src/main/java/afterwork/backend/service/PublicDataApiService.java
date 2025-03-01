65655e8ea1fb849ea708d9ccd13e9debb6cf8339
YeDongVibe <wkwhstladmss@naver.com>
2025-03-01 14:33:17 +0900

    250301_API ing

--- backend/src/main/java/afterwork/backend/service/PublicDataApiService.java
+++ backend/src/main/java/afterwork/backend/service/PublicDataApiService.java
@@ -2,7 +2,9 @@ package afterwork.backend.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import afterwork.backend.dto.PublicDataDto;
 import afterwork.backend.mapper.PublicDataMapper;
 
 @Service
@@ -11,10 +13,15 @@ public class PublicDataApiService {
     private final PublicDataMapper publicDataMapper;
 
     private final String API_URL = "http://apis.data.go.kr/B551011/KorService1";
-    private final String SERVICE_KEY = "VVOmSStfN2wJSHEf%2BdtpJXmiBOUek6SBLCa3ue%2BfX1qCYUMmwRU13VFK6bcrNTm6XpOMTYs1Z77cRmRno%2BYqkg%3D%3D";
+    //디코딩키
+    private final String SERVICE_KEY = "VVOmSStfN2wJSHEf+dtpJXmiBOUek6SBLCa3ue+fX1qCYUMmwRU13VFK6bcrNTm6XpOMTYs1Z77cRmRno+Yqkg==";
 
     public PublicDataApiService(RestTemplate restTemplate, PublicDataMapper publicDataMapper) {
         this.restTemplate = restTemplate;
         this.publicDataMapper = publicDataMapper;
+    }
+
+    public List<PublicDataDto> fetchAndSavePublicData () {
+        String url = UriComponentsBuilder.fromHttpUrl(API_URL)
     }
 }
